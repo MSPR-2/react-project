7b112a2f77e87c67948b23f093b707d0a5083178
Aurélien Riche <contact@aurelienriche.com>
2020-12-27 22:59:34 +0100

    set fail UT

--- src/services/todo.test.js
+++ src/services/todo.test.js
@@ -4,3 +4,7 @@ import { getAll, getItemById, updateStatus } from './todo';
 test('should return true', () => {
     expect(true).toBeTruthy();
 });
+
+test('should return false', () => {
+    expect(false).toBeTruthy();
+});
