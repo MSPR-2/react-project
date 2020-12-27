a77e5152fd3b1e81559d975b25e9a64e74b78900
Aurélien Riche <contact@aurelienriche.com>
2020-12-27 16:03:10 +0100

    test ci

--- src/services/todo.test.js
+++ src/services/todo.test.js
@@ -4,3 +4,7 @@ import { getAll, getItemById, updateStatus } from './todo';
 test('should return true', () => {
     expect(true).toBeTruthy();
 });
+
+test('should return true 2', () => {
+    expect(true).toBeTruthy();
+});
