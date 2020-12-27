b0dab611967f0e428dd3bbcda2f98cc0171e50e4
Aurélien Riche <contact@aurelienriche.com>
2020-12-27 19:17:39 +0100

    Revert "fix uts"

--- src/services/todo.test.js
+++ src/services/todo.test.js
@@ -4,3 +4,11 @@ import { getAll, getItemById, updateStatus } from './todo';
 test('should return true', () => {
     expect(true).toBeTruthy();
 });
+
+test('should return true 2', () => {
+    expect(true).toBeTruthy();
+});
+
+test('should return true 3', () => {
+    expect(true).toBeTruthy();
+});
