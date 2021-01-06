ba2280c1495238b8c97d7fd8e67a340f29fdb015
Alex <alexandre.dessolle@gmail.com>
2021-01-06 11:40:53 +0100

    mode service UT

--- src/services/mode.test.js
+++ src/services/mode.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { getNextModeByKey } from './mode';
 
-test('should return true', () => {
-    expect(true).toBeTruthy();
+test('test none', () => {
+    expect('none').toEqual(getNextModeByKey('none', 27));
 });
 
-test('should return true', () => {
-    expect(true).toBeTruthy();
+test('test search', () => {
+    expect('search').toEqual(getNextModeByKey('none', 191));
+});
+
+test('test create', () => {
+    expect('create').toEqual(getNextModeByKey('none', 78));
 });
